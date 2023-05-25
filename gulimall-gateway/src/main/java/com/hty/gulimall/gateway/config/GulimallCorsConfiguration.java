161a9c1d238802aa207eb27c831d1bfafca7cdf8
huangtianyou <1156388927@qq.com>
2023-05-25 16:41:21 +0800

    添加CORS的配置

---
+++ gulimall-gateway/src/main/java/com/hty/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -0,0 +1,31 @@
+package com.hty.gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author hty
+ * @date 2023-05-25 16:36
+ * @email 1156388927@qq.com
+ * @description
+ * @other 更多请看www.autunomy.top
+ */
+
+@Configuration
+public class GulimallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("*");
+        configuration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**",configuration);
+        return new CorsWebFilter(source);
+    }
+}
