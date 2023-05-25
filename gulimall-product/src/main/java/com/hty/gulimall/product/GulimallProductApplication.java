4f8718cda69eb798c6db5704f977f0a2f7897b10
huangtianyou <1156388927@qq.com>
2023-05-25 17:04:27 +0800

    完成后端的三级菜单的查询功能

--- gulimall-product/src/main/java/com/hty/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/hty/gulimall/product/GulimallProductApplication.java
@@ -3,8 +3,9 @@ package com.hty.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @MapperScan("com.hty.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
